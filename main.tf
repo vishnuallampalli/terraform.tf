d87012b1676e30146c92001ad069c2090681ad8b
vishnuallampalli <112451766+vishnuallampalli@users.noreply.github.com>
2023-05-29 13:15:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 provider "aws" {
   region = "ap-south-1"
 }
+
 # Create VPC
 resource "aws_vpc" "k8s_vpc" {
   cidr_block = "10.1.0.0/16"
@@ -9,6 +10,7 @@ resource "aws_vpc" "k8s_vpc" {
     Name = "k8s-vpc"
   }
 }
+
 # Create Subnets
 resource "aws_subnet" "k8s_subnet" {
   vpc_id            = aws_vpc.k8s_vpc.id
@@ -32,10 +34,12 @@ resource "aws_route_table" "example" {
     gateway_id = aws_internet_gateway.example.id
   }
 }
+
 resource "aws_route_table_association" "example" {
   subnet_id      = aws_subnet.k8s_subnet.id
   route_table_id = aws_route_table.example.id
 }
+
 # Create Security Group for Kubernetes nodes
 resource "aws_security_group" "k8s_sg" {
   vpc_id = aws_vpc.k8s_vpc.id
@@ -46,12 +50,14 @@ resource "aws_security_group" "k8s_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   # Add any additional ingress rules as needed
 
   egress {
@@ -65,6 +71,7 @@ resource "aws_security_group" "k8s_sg" {
     Name = "k8s-security-group"
   }
 }
+
 # Create EC2 Instances for Kubernetes master and worker nodes
 resource "aws_instance" "k8s_master" {
   ami                         = "ami-0f5ee92e2d63afc18"
@@ -73,6 +80,7 @@ resource "aws_instance" "k8s_master" {
   subnet_id                   = aws_subnet.k8s_subnet.id
   vpc_security_group_ids      = [aws_security_group.k8s_sg.id]
   associate_public_ip_address = true
+
   provisioner "local-exec" {
     command = "sleep 30"
   }
@@ -94,40 +102,34 @@ resource "aws_instance" "k8s_node1" {
     command = "sleep 30"
   }
 
-
   tags = {
     Name = "k8s_node1"
   }
 }
 
-
 output "k8s_master" {
   value = aws_instance.k8s_master.public_ip
 }
+
 output "k8s_node1" {
   value = aws_instance.k8s_node1.public_ip
 }
 
 resource "null_resource" "inventory_creation" {
-
   depends_on = [
     aws_instance.k8s_master,
     aws_instance.k8s_node1
   ]
+
   provisioner "local-exec" {
     command = <<EOT
-sudo -S sh -c 'cat <<EOF > /etc/ansible/hosts
-k8s_master ansible_host=${aws_instance.k8s_master.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/home/ubuntu/jenkins-master.pem ansible_ssh_extra_args="-o StrictHostKeyChecking=accept-new"
-k8s_node1 ansible_host=${aws_instance.k8s_node1.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/home/ubuntu/jenkins-master.pem ansible_ssh_extra_args="-o StrictHostKeyChecking=accept-new"
-EOF'
+      sudo sh -c 'cat <<EOF > /etc/ansible/hosts
+      [k8s_master]
+      k8s_master ansible_host=${aws_instance.k8s_master.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/home/ubuntu/jenkins-master.pem ansible_ssh_extra_args="-o StrictHostKeyChecking=accept-new"
+
+      [k8s_node1]
+      k8s_node1 ansible_host=${aws_instance.k8s_node1.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/home/ubuntu/jenkins-master.pem ansible_ssh_extra_args="-o StrictHostKeyChecking=accept-new"
+      EOF'
     EOT
   }
 }
-  
-  
-
-
-
-
-
-
