18b961696a9cd6b8623df7b81589d52b6bd2a0ab
vishnuallampalli <112451766+vishnuallampalli@users.noreply.github.com>
2023-05-31 22:46:16 +0530

    Update var.tf

--- terra3/var.tf
+++ terra3/var.tf
@@ -1,14 +1,14 @@
-variable "aws_access_key" {
- description = "AWS Access Key"
- type = string
-}
+#variable "aws_access_key" {
+# description = "AWS Access Key"
+ #type = string
+#}
 
 
 
-variable "aws_secret_key" {
- description = "AWS Secret Key"
- type = string
-}
+#variable "aws_secret_key" {
+# description = "AWS Secret Key"
+# type = string
+#}
 
 
 
