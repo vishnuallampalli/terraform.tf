4079f0e42c16211792a821f0a2a664116ae7cca7
vishnuallampalli <112451766+vishnuallampalli@users.noreply.github.com>
2023-05-31 22:47:42 +0530

    Update output.tf

--- terra3/modules/ec2_instance/output.tf
+++ terra3/modules/ec2_instance/output.tf
@@ -1,7 +1,7 @@
-output "ec2_instance_id" {
- description = "ID of the EC2 instance"
- value = aws_instance.example.id
-}
+#output "ec2_instance_id" {
+# description = "ID of the EC2 instance"
+# value = aws_instance.example.id
+#}
 
 
 output "k8s_master" {
