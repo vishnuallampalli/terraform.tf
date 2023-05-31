bdf29e83f3f10b2e84215a7dfd711e2c2ee17f2e
vishnuallampalli <112451766+vishnuallampalli@users.noreply.github.com>
2023-05-31 20:33:09 +0530

    Add files via upload

---
+++ terra3/modules/ec2_instance/output.tf
@@ -0,0 +1,13 @@
+output "ec2_instance_id" {
+ description = "ID of the EC2 instance"
+ value = aws_instance.example.id
+}
+
+
+output "k8s_master" {
+  value = aws_instance.k8s_master.public_ip
+}
+
+output "k8s_node1" {
+  value = aws_instance.k8s_node1.public_ip
+}
